4e87cfb08d8b8cd2178d7f184f983f6e9dac6563
ValtsuL1 <linnavaltteri@gmail.com>
2024-04-04 09:44:27 +0300

    added LoginScreen

--- app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/MainActivity.kt
+++ app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/MainActivity.kt
@@ -1,21 +1,30 @@
 package com.example.edistynyt_mobiiliohjelmointi
 
 import android.os.Bundle
+import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.edistynyt_mobiiliohjelmointi.ui.theme.Edistynyt_mobiiliohjelmointiTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,9 +32,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.Red
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginScreen()
                 }
             }
         }
@@ -33,17 +42,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Edistynyt_mobiiliohjelmointiTheme {
-        Greeting("Android")
+fun LoginScreen(){
+    val username = remember{
+        mutableStateOf("")
+    }
+    val password = remember {
+        mutableStateOf("")
+    }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(value = username.value, onValueChange = { newUsername -> username.value = newUsername
+        }, placeholder = {
+            Text(text = "Username")
+        })
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = password.value, onValueChange = { newPassword -> password.value = newPassword
+        }, placeholder = {
+            Text(text = "Password")
+        })
     }
 }
