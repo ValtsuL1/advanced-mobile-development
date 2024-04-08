88250361ca72fecee1c6e5c77a652afae078223d
ValtsuL1 <linnavaltteri@gmail.com>
2024-04-08 16:34:07 +0300

    added navigation drawer

--- app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/MainActivity.kt
+++ app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/MainActivity.kt
@@ -1,32 +1,37 @@
 package com.example.edistynyt_mobiiliohjelmointi
 
 import android.os.Bundle
-import android.view.Gravity
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import com.example.edistynyt_mobiiliohjelmointi.ui.theme.Edistynyt_mobiiliohjelmointiTheme
-import com.example.edistynyt_mobiiliohjelmointi.viewmodel.LoginViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,7 +43,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen()
+                    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                    val scope = rememberCoroutineScope()
+                    ModalNavigationDrawer(
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding),
+                        drawerContent = {
+                            ModalDrawerSheet {
+                                Spacer(modifier = Modifier.height(16.dp))
+                                NavigationDrawerItem(
+                                    label = { Text(text = "Categories") },
+                                    selected = true,
+                                    onClick = { /*TODO*/ },
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Filled.Home,
+                                            contentDescription = "Home")
+                                    })
+                            }
+                        }, drawerState = drawerState) {
+                        Text(text = "Welcome Home")
+                    }
                 }
             }
         }
@@ -46,26 +70,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginScreen() {
-    val loginVm: LoginViewModel = viewModel()
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        OutlinedTextField(value = loginVm.loginState.value.username, onValueChange = { newUsername ->
-            loginVm.setUsername(newUsername)
-        }, placeholder = {
-            Text(text = "Username")
-        })
-        Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = loginVm.loginState.value.password, onValueChange = { newPassword ->
-            loginVm.setPassword(newPassword)                                                                         
-        }, placeholder = {
-            Text(text = "Password")
-        }, visualTransformation = PasswordVisualTransformation())
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { loginVm.login() }) {
-            Text(text = "Login")
+fun AppNavigation(navController: NavHostController, onIconButtonClick: () -> Unit) {
+    NavHost(navController = navController, startDestination = "homescreen") {
+        composable(route = "homescreen") {
+            HomeScreen(onIconButtonClick)
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(onIconButtonClick: () -> Unit) {
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home") },
+                navigationIcon = {
+                    IconButton(onClick = { onIconButtonClick() }) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menu")
+                    }
+                })
+        },
+    ){
+        Text(text = "Home", modifier = Modifier.padding((it)))
+    }
+}
+
