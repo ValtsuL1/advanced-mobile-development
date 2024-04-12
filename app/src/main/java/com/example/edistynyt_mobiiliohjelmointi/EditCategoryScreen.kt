f84b83561d664e8765f19e5788fc237e8b178bbf
ValtsuL1 <linnavaltteri@gmail.com>
2024-04-12 13:16:50 +0300

    added CategoryViewModel

--- app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/EditCategoryScreen.kt
+++ app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/EditCategoryScreen.kt
@@ -1,2 +1,10 @@
 package com.example.edistynyt_mobiiliohjelmointi
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditCategoryScreen(backToCategories: () -> Unit, goToCategories: () -> Unit) {
+    val vm: Cate
+}
---
+++ app/src/main/java/com/example/edistynyt_mobiiliohjelmointi/viewmodel/CategoryViewModel.kt
@@ -0,0 +1,80 @@
+package com.example.edistynyt_mobiiliohjelmointi.viewmodel
+
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.edistynyt_mobiiliohjelmointi.api.categoriesService
+import com.example.edistynyt_mobiiliohjelmointi.model.CategoryState
+import com.example.edistynyt_mobiiliohjelmointi.model.EditCategoryReq
+import kotlinx.coroutines.launch
+
+class CategoryViewModel(savedStateHandle: SavedStateHandle): ViewModel() {
+    private val _categoryId = savedStateHandle.get<String>("categoryId")?.toIntOrNull() ?: 0
+
+    private val _categoryState = mutableStateOf(CategoryState())
+    val categoryState: State<CategoryState> = _categoryState
+
+    fun setOk(status: Boolean) {
+        _categoryState.value = _categoryState.value.copy(ok = status)
+    }
+
+    fun editCategory() {
+        viewModelScope.launch {
+            try {
+                _categoryState.value = _categoryState.value.copy(loading = true)
+                categoriesService.editCategory(
+                    _categoryId,
+                    EditCategoryReq(name = _categoryState.value.item.name)
+                )
+                setOk(true)
+            } catch (e: Exception) {
+                _categoryState.value = _categoryState.value.copy(err = e.toString())
+            } finally {
+                _categoryState.value = _categoryState.value.copy(loading = false)
+            }
+        }
+    }
+
+    private fun getCategory() {
+        viewModelScope.launch {
+            try {
+                _categoryState.value = _categoryState.value.copy(loading = true)
+                val res = categoriesService.getCategory(_categoryId)
+                _categoryState.value = _categoryState.value.copy(item = res.category)
+            } catch (e: Exception) {
+                _categoryState.value = _categoryState.value.copy(err = e.toString())
+            } finally {
+                _categoryState.value = _categoryState.value.copy(loading = false)
+            }
+        }
+    }
+
+    init {
+        getCategory()
+    }
+
+    fun setName(newName: String) {
+        val item = _categoryState.value.item.copy(name = newName)
+        _categoryState.value = _categoryState.value.copy(item = item)
+    }
+
+    fun editCategory(goToCategories: () -> Unit) {
+        viewModelScope.launch {
+            try {
+                _categoryState.value = _categoryState.value.copy(loading = true)
+                categoriesService.editCategory(
+                    _categoryId,
+                    EditCategoryReq(name = _categoryState.value.item.name)
+                )
+                goToCategories()
+            } catch (e: Exception) {
+                _categoryState.value = _categoryState.value.copy(err = e.toString())
+            } finally {
+                _categoryState.value = _categoryState.value.copy(loading = false)
+            }
+        }
+    }
+}
